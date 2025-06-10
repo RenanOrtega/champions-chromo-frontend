925e7ccb1924a20a8aee0d6e42e6c8e624a7903d
renan-disparopro <renan@disparopro.com.br>
2025-06-10 19:43:12 -0300

    fix: remove text.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
              style={{ backgroundImage: "url('/logo.png')" }}></div>
         <div className="relative z-10">
           <h1 className="text-3xl md:text-4xl font-bold mb-4">
-            Álbuns de Figurinhas Personalizados para sua Escola
+            Álbuns de Figurinhas Personalizados
           </h1>
           <p className="text-lg mb-6">
             Quer completar seu álbum de figurinhas?
